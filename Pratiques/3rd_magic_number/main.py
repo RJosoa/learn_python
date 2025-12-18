245e60ee6605efa30e5a0f2e4268e1677e5bde5d
Josoa Rafarilahinavalona <josoa@Josoas-MacBook-Pro.local>
2025-12-18 13:05:38 +0100

    add loop and life ussing while or for loop

--- Pratiques/3rd_magic_number/main.py
+++ Pratiques/3rd_magic_number/main.py
@@ -1,31 +1,67 @@
+import random
+
+
 NB_MIN = 1
 NB_MAX = 10
-NB_MAGIC = 5
+NB_MAGIC = random.randint(NB_MIN, NB_MAX)
+NB_VIES = 4
 
-def condition_nombre(nb_magic, nb_str):
-    try :
-        nb = int(nb_str)
-        if nb == nb_magic:
-            print("Bravo, vous avez gagné")
-        elif nb > nb_magic  :
-            print("Le nombre magic est plus petit")
+def demander_nombre(nb_min, nb_max):
+    nb_int = 0
+    while nb_int == 0 :
+        user_input = input(f"Quel est le nombre magic ( entre {nb_min} et {nb_max} ) ? : ")
+        try :
+            nb_int = int(user_input)
+            
+        except ValueError:
+            print("UN NOMBRE PARDIS !!!")
+            
         else :
-            print("Le nombre magic est plus grand")        
-    except ValueError :
-        print("UN NOMBRE PARDIS !!!")
+            if nb_int < nb_min or nb_int > nb_max :
+                print(f"Veuillez entrer un nombre entre {nb_min} et {nb_max}")
+                nb_int = 0
+    
+    return nb_int
 
+"""
+--- Boucle While pour les vies ---
+vies = NB_VIES
+nb = 0
+while not nb == NB_MAGIC and not vies == 0 :
+    print(f"Vouz avez {vies} vies")
+    nb = demander_nombre(NB_MIN, NB_MAX)
+    
+    if nb == NB_MAGIC:
+        print("Bravo, vous avez gagné")
+    elif nb > NB_MAGIC:
+        print("Le nombre magic est plus petit")
+        vies -= 1
+    else:
+        print("Le nombre magic est plus grand")
+        vies -= 1
 
+if vies == 0 :
+    print("Vous avez perdu")
+    
+"""
 
-def demander_nombre (nb_min, nb_max) :
-    nb = 0
-    while nb != NB_MAGIC:
+win = False
+
+for i in range(0, NB_VIES):
+    
+    nb = demander_nombre(NB_MIN, NB_MAX)
         
-        nb = input(f"Quel est le nombre magic ( entre {nb_min} et {nb_max} ) ? : ")
-
-        condition_nombre(NB_MAGIC, nb)
-
-
+    if nb == NB_MAGIC:
+            print("Bravo, vous avez gagné")
+            win = True
+            break
+            
+    elif nb > NB_MAGIC:
+            print("Le nombre magic est plus petit")
     
-    
-demander_nombre(NB_MIN, NB_MAX)
+    else:
+            print("Le nombre magic est plus grand")
 
+
+if win :
+    print("Vous avez perdu")
