02f17a9ec9c742999244c7eb2c21ef88c2ee6a1a
Josoa Rafarilahinavalona <josoa@Josoas-MacBook-Pro.local>
2025-12-19 14:39:30 +0100

    exam 1 finished

---
+++ EXAM/1_Base/main.py
@@ -0,0 +1,87 @@
+TO_CM = 2.54
+TO_INCHES = 0.394
+
+def conversion() :
+    print("--- Convesion entre cm et pouces ---")
+    print("Rappelle :")
+    print(f"1 pouce = {TO_CM} cm")
+    print(f"1 cm = {TO_INCHES} pouce")
+    print()
+    print("Qu'est ce que vous voulez convertir ? :")
+    print("n°1 : pouces vers cm ?")
+    print("n°2 : cm vers pouces ?")
+    
+    num_int = 0
+    while num_int == 0  :
+        user_input = input("-> n°")
+        try :
+            num_int = int(user_input)
+            if num_int < 1 or num_int > 2 :
+                print()
+                print("Veuillez choisir 1 ou 2 !")
+                num_int = 0
+                
+        except ValueError: 
+            print()
+            print("Veuillez ecrire un nombre !")
+                
+    print()
+    value_int = 0
+    while value_int == 0 :
+        print("La valeur a convertir : ")
+        value_str = input("-> ")
+        try :
+            value_int = float(value_str)
+            
+        except ValueError: 
+            print("Erreur. Veuillez choisir des chiffres et utilisez le . au lieu d'une ,")
+        
+        print()
+        
+    if num_int == 1 :
+        print(f"Convertion de {value_int} pouces vers cm")
+        result = float(value_int * TO_CM)
+        result_round = round(result, 2)
+        return print(f"{value_int} pouces = {result_round} cm")
+            
+    elif num_int == 2 :
+        print(f"Convertion de {value_int} cm vers pouces")
+        result = float(value_int * TO_INCHES)
+        result_round = round(result, 2)
+        return print(f"{value_int} cm = {result_round} pouces")
+            
+    else :
+        print("Error")
+        
+    
+
+restart = True
+while restart :
+    conversion()
+    print()
+    print("Un autre convertion ?")
+    print("répondez par o (oui) ou n (non)")
+    
+    x = 0
+    while x == 0 :
+        response = input("-> ")
+        try : 
+            if response == "o" or response ==  "oui" or response == "o (oui)" or response ==  "o(oui)" :
+                print("oui")
+                corect = True
+                
+            elif response == "n" or response == "non" or response == "n (non)" or response == "n(non)" :
+                print("non")
+                restart = False
+                break
+                
+            else :
+                print("soit o ou n")
+                x = 0
+                print()
+        
+        except :
+            print("ERROR")   
+    
+    print()     
+    print ("// A bientôt // 👋")
---
+++ EXAM/1_Base/readme.md
@@ -0,0 +1,33 @@
+Pour cet exercice, vous allez créer un programme de conversion d'unités, qui sera capable de convertir des pouces (inch) en centimètres (cm), et vice-versa.
+
+1 pouce = 2.54 cm
+1 cm = 0.394 pouces
+
+Exemple :
+Un écran de 17 pouces de diagonale, correspond à 43,18 cm (=17*2.54)
+
+Voici comment votre programme doit se comporter :
+1 - Demander à l'utilisateur si il souhaite convertir de "pouces vers cm" ou "cm vers pouces"
+2 - Demander à l'utilisateur de rentrer la valeur à convertir (en réaffichant l’unité demandée)
+3 - Afficher la valeur convertie (et l'unité : cm ou pouces)
+- fin du programme.
+
+Nombres à virgule :
+Nous avons vu comment manipuler des données numériques avec Python, notamment avec le type "int" qui concerne les nombres entiers (sans virgules).
+Pour cet exercice, vous allez être amené à manipuler des nombres à virgule. Il s'agit du type "float".
+Le principe du float est similaire à celui du int.
+Pour utiliser un nombre à virgule, il faut utiliser le point (et non une virgule).
+Exemples :
+a = 5  # int
+b = 5.2 # float
+c = 5,2 # erreur : ne pas utiliser de virgule, mais le point
+
+Optionnel :
+Boucler pour convertir plusieurs valeurs (en conservant toujours le même sens de conversion), et proposer une option pour sortir du programme.
+
+Votre réponse :
+Codez avec l'éditeur de votre choix, puis faites un copier/coller de votre code directement dans le champ de la réponse.
+Questions pour cet exercice
+
+Collez directement votre code ici, en utilisant le formatage <> code.
+OPTIONNEL : Dites moi ici vos remarques : avez-vous aimé cet exercice ? Est-ce que c'était difficile/facile ? Souhaitez-vous d'autres exercices de ce type ? Combien de temps ça vous a pris ?
